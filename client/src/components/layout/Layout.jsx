c124cc2ce9baeee737bd061f55156a56ecf6d82f
Jeremy.R <119907355+Nokkt@users.noreply.github.com>
2025-06-06 11:33:35 +0800

    Update Layout.jsx

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import  Header  from "./Header";
-import Sidebar  from "./Sidebar";
+import  Header  from "./header";
+import Sidebar  from "./sidebar";
 
 export const Layout = ({ children }) => {
   return (
